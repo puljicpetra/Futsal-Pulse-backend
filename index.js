2d48a1415ab0075c7cf8385f70d424396722a25a
Petra Puljić <ppuljicc@gmail.com>
2025-02-06 18:49:22 +0100

    Konfiguriran CORS

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Backend radi!');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server radi na http://localhost:${PORT}`);
 });
